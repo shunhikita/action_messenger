1a99fe35562202e8050e9c538a04fd527faa30ff
shun.hikita <hikita@counterworks.jp>
2018-07-21 17:57:02 +0900

    back railtie

--- lib/action_messenger/railtie.rb
+++ lib/action_messenger/railtie.rb
@@ -1,9 +1,9 @@
-# require 'action_messenger'
-# require 'rails'
-#
-# module ActionMessenger
-#   class Railtie < Rails::Railtie
-#     config.action_messenger = ActiveSupport::OrderedOptions.new
-#     config.eager_load_namespaces << ActionMessenger
-#   end
-# end
+require 'action_messenger'
+require 'rails'
+
+module ActionMessenger
+  class Railtie < Rails::Railtie
+    config.action_messenger = ActiveSupport::OrderedOptions.new
+    config.eager_load_namespaces << ActionMessenger
+  end
+end
